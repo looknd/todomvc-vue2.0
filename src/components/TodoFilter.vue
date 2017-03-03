a097bc1b6ae0a2a214cc8f7604b7acb48a60d245
lxynox <lxynox@gmail.com>
2017-03-02 21:19:57 -0500

    all vue components

---
+++ src/components/TodoFilter.vue
@@ -0,0 +1,33 @@
+<template>
+  <footer class="footer" v-show="todos.length">
+    <span class="todo-count">
+      <strong v-text="remaining"></strong> {{pluralize('item', remaining)}} left
+    </span>
+    <ul class="filters">
+      <li><a href="#/all" :class="{selected: visibility == 'all'}">All</a></li>
+      <li><a href="#/active" :class="{selected: visibility == 'active'}">Active</a></li>
+      <li><a href="#/completed" :class="{selected: visibility == 'completed'}">Completed</a></li>
+    </ul>
+    <button class="clear-completed" @click="removeCompleted" v-show="todos.length > remaining">
+      Clear completed
+    </button>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: 'todo-filter',
+  props: ['todos', 'remaining', 'visibility'],
+
+  methods: {
+    removeCompleted() {
+      this.$emit('removeCompleted')
+    },
+    pluralize(word, count) {
+      return word + (count === 1 ? '' : 's');
+    }
+  }
+}
+</script>
+
+<style></style>
